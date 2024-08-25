90bcdd89e01b59113c6fec7028ca6f39dbfd49ba
kayumov1554 <167546948+kayumov1554@users.noreply.github.com>
2024-08-25 22:28:04 +0500

    1

---
+++ ready-html/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Faylga kerakli ruxsatlarni berish
+chmod 755 ready-html/index.html
+
+# Build jarayonini boshlash (bu yerda o‘zingizning build buyruqlarini kiriting)
+# Misol uchun, agar siz `npm start` yoki boshqa build buyrug‘idan foydalansangiz:
+npm start
